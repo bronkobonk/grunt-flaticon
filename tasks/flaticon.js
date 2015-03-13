a1c2c09ce46a46a63da180fd81c5d0e093ddee86
Bronisław Białek <bronislaw.bialek@gowork.pl>
2015-03-13 15:47:13 +0100

    extract flaticon module

--- tasks/flaticon.js
+++ tasks/flaticon.js
@@ -4,9 +4,7 @@
  * Copyright (c) 2015 Marcin Bonk, Bronisław Białek
  * Licensed under the MIT license.
  */
-/**
- * @param grunt
- */
+
 module.exports = function (grunt) {
     'use strict';
 
@@ -14,11 +12,12 @@ module.exports = function (grunt) {
     var unzip = require('unzip');
     var fs = require('fs');
     var path = require('path');
+    var flaticon = require('./lib/flaticon');
 
     grunt.registerMultiTask(
-        'flaticon', 'Grunt task to automatically download flaticon icons collection from config stored in JSON file',
-
-        function flaticon() {
+        'flaticon',
+        'Grunt task to automatically download flaticon icons collection from config stored in JSON file',
+        function flaticonTask() {
             var self = this;
             var files = this.files;
             var done = this.async();
@@ -31,75 +30,8 @@ module.exports = function (grunt) {
                 styles: 'css'
             });
 
-            var configJson = typeof options.config === "string" ? grunt.file.readJSON(options.config) : options.config;
-            var data = {data: JSON.stringify(configJson.icons), acc: "font", scode: "4", uId: 0};
-
-            needle.post(options.url, data, {multipart: true}, function (err, res, body) {
-                if (err) {
-                    grunt.log.error();
-                    done();
-                } else {
-                    grunt.log.ok();
-                    grunt.log.debug('sid: ' + body);
-                    handlePackageHash(body);
-                }
-            });
-            /**
-             * @param templateName
-             * @param config
-             */
-            function generateCSS(config) {
-                var file = grunt.file.read('templates/flaticon.css');
-                console.log(typeof file);
-
-                console.log(typeof config);
-                grunt.template.process(file, config);
-            }
-
-            /**
-             * @param id
-             */
-            function handlePackageHash(id) {
-                grunt.log.write('Fetching archive ' + options.url_package + id + '...');
-
-                var request = needle.get(options.url_package + id, function (err, res, body) {
-                    if (err) {
-                        done();
-                        grunt.log.err();
-                    }
-                });
-
-                request.pipe(unzip.Parse())
-
-                    .on('entry', function (entry) {
-                        var ext = path.extname(entry.path);
-
-                        if (entry.type === 'File') {
-                            switch (ext) {
-                                case '.woff':
-                                case '.svg':
-                                case '.ttf':
-                                case '.eot':
-                                    var fontPath = path.join(options.fonts, path.basename(entry.path));
-                                    return entry.pipe(fs.createWriteStream(fontPath));
-
-                                case '.css':
-                                    var fontPath = path.join(options.styles, path.basename(entry.path));
-                                    generateCSS(configJson);
-                                    return entry.pipe(fs.createWriteStream(fontPath));
-
-                                default:
-                                    grunt.verbose.writeln('Ignored ', entry.path);
-                                    entry.autodrain();
-                            }
-                        }
-                    })
-
-                    .on('finish', function () {
-                        grunt.log.ok();
-                        done();
-                    });
-            }
+            var fl = new flaticon(options);
+            fl.downloadZip(done);
         }
     );
 };
---
+++ tasks/lib/flaticon.js
@@ -0,0 +1,84 @@
+/*
+ * grunt-flaticon
+ *
+ * Copyright (c) 2015 Marcin Bonk, Bronisław Białek
+ * Licensed under the MIT license.
+ */
+
+module.exports = (function () {
+    'use strict';
+    
+    var needle = require('needle');
+    var unzip = require('unzip');
+    var fs = require('fs');
+    var path = require('path');
+    var grunt = require('grunt');
+    
+    function flaticon(options) {
+        this.options = options;
+        this.config = typeof options.config === "string" ? grunt.file.readJSON(options.config) : options.config;
+    }
+
+    flaticon.prototype.downloadZip = function downloadZip(done) {
+        var data = {data: JSON.stringify(this.config.icons), acc: "font", scode: "4", uId: 0};
+
+        needle.post(this.options.url, data, { multipart: true }, function (err, res, body) {
+            if (err) {
+                grunt.verbose.writeln('fetching error: ' + err);
+                done();
+            } else {
+                grunt.verbose.writeln('sid: ' + body);
+                this.handlePackageHash(body, done);
+            }
+        }.bind(this));
+    };
+
+    flaticon.prototype.handlePackageHash = function handlePackageHash(id, done) {
+        grunt.log.write('Fetching archive ' + this.options.url_package + id + '...');
+
+        var request = needle.get(this.options.url_package + id, function (err, res, body) {
+            if (err) {
+                done();
+                grunt.log.err();
+            }
+        });
+
+        request.pipe(unzip.Parse())
+
+        .on('entry', function (entry) {
+            var ext = path.extname(entry.path);
+
+            if (entry.type === 'File') {
+                switch (ext) {
+                    case '.woff':case '.svg': case '.ttf': case '.eot':
+                    var fontPath = path.join(this.options.fonts, path.basename(entry.path));
+                    return entry.pipe(fs.createWriteStream(fontPath));
+
+                    case '.css':
+                        var fontPath = path.join(this.options.styles, path.basename(entry.path));
+                        return entry.pipe(fs.createWriteStream(fontPath));
+
+                    default:
+                        grunt.verbose.writeln('Ignored ', entry.path);
+                        entry.autodrain();
+                }
+            }
+        }.bind(this))
+
+        .on('finish', function() {
+            this.generateCSS();
+            grunt.log.ok();
+            done();
+        }.bind(this));
+    };
+    
+    flaticon.prototype.generateCSS = function generateCSS() {
+        var file = grunt.file.read(__dirname + '/../../templates/flaticon.css');
+        console.log(typeof file);
+
+        console.log(typeof this.config);
+        grunt.template.process(file, this.config);
+    };
+    
+    return flaticon;
+})();
